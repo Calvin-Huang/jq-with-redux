47865c3fcffd48e218799570d540b83a3df74935
Calvin Huang <calvin.peak@capslock.tw>
2017-09-03 16:45:33 -0700

    Add root model.

---
+++ models/application-record.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+const config = require('config/config')[process.env.NODE_ENV || 'development'];
+
+config.define = {
+  underscored: true,
+};
+
+module.exports = new Sequelize(config);
