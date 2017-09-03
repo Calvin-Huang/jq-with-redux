24c0a79113767d3b05e7b3880f02ce2b2e6efbc5
Calvin Huang <calvin.peak@capslock.tw>
2017-09-03 16:55:03 -0700

    Add simple API for listing bookmark.

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const path = require('path');
 const express = require('express');
 const server = express();
 
+const Bookmark = require('./models/bookmark');
+
 server.use('/views', express.static(path.resolve(__dirname, 'public', 'views')));
 
 server.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
+const apiV1Route = express.Router();
+apiV1Route.get('/bookmarks', async (req, res) => {
+  const bookmarks = await Bookmark.findAll();
+  res.json(bookmarks);
+});
+
+server.use('/api/v1', apiV1Route);
 
 server.listen('3000', () => {
   console.log('> Server is ready on port 3000.')
