d7101dfd78fdd6351d31ddbc3b37e1bf51537bbf
Calvin Huang <calvin.peak@capslock.tw>
2017-09-03 15:08:14 -0700

    Change repo item to a standalone template.

---
+++ public/repo.js
@@ -0,0 +1,35 @@
+$.templates('repo', `
+  <img width="34" height="34" src="https://avatars2.githubusercontent.com/u/509841?v=4&s=64" />
+  <div class="repo-info">
+    <h3 class="mb-1">
+      <a href="/libgdx/libgdx">
+        <span class="text-normal">libgdx / </span>libgdx
+      </a>
+    </h3>
+    <div class="d-flex">
+      <p>Desktop/Android/HTML5/iOS Java game development framework</p>
+      <a href="#" class="bookmark" data-id="1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z" />
+        </svg>
+      </a>
+    </div>
+    <div class="text-gray">
+      <span class="mr-3">
+        Java
+      </span>
+      <a class="muted-link d-inline-block mr-3" href="/libgdx/libgdx/stargazers">
+        <svg aria-label="star" class="octicon octicon-star" height="16" role="img" version="1.1" viewBox="0 0 14 16" width="14">
+          <path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path>
+        </svg>
+        12,317
+      </a>
+      <a class="muted-link d-inline-block mr-3" href="/libgdx/libgdx/network">
+        <svg aria-label="fork" class="octicon octicon-repo-forked" height="16" role="img" version="1.1" viewBox="0 0 10 16" width="10">
+          <path fill-rule="evenodd" d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path>
+        </svg>
+        5,392
+      </a>
+    </div>
+  </div>
+`);
