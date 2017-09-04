818a07a955daeb57e5d791e4662069161a04232a
Calvin Huang <calvin.peak@capslock.tw>
2017-09-03 17:12:25 -0700

    Fix migration.

--- migrations/20170904000921-store_bookmark_with_full_name.js
+++ migrations/20170904000921-store_bookmark_with_full_name.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.addColumn('bookmarks', 'full_name', { type: Sequelize.STRING, allowNull: false });
+    return queryInterface.addColumn('bookmarks', 'full_name', { type: Sequelize.STRING, allowNull: true });
   },
 
   down: function (queryInterface, Sequelize) {
