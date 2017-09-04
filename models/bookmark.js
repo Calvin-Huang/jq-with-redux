e3bd2cd60e0a67cc7d887e4ecc2f2f0b2b3e1dbf
Calvin Huang <calvin.peak@capslock.tw>
2017-09-03 17:11:34 -0700

    Store repo's full name.

--- models/bookmark.js
+++ models/bookmark.js
@@ -3,4 +3,5 @@ const ApplicationRecord = require('./application-record');
 
 module.exports = ApplicationRecord.define('bookmark', {
   repo_id: Sequelize.STRING,
+  full_name: Sequelize.STRING,
 }, {});
