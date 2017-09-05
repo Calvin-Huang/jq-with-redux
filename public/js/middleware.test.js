c7ec361dcbeb062db42226a6a821f52554139602
Calvin Huang <calvin.peak@capslock.tw>
2017-09-04 21:53:24 -0700

    Move jQuery mock to higher level.

--- public/js/middleware.test.js
+++ public/js/middleware.test.js
@@ -6,17 +6,16 @@ import actions, { types } from './actions';
 import util from 'util';
 
 describe('test middleware behavior', () => {
+  // Mock jQuery.
+  class jQuery {
+    constructor(selector, context) { }
+    html() { }
+  }
+  const $ = (selector, context) => new jQuery(selector, context);
+  $.templates = () => ({ render: () => { } });
+
   describe('when the network request is fine', () => {
-
-    // Mock jQuery.
-    class jQuery {
-      constructor(selector, context) {}
-      html() {}
-    }
-    const $ = (selector, context) => new jQuery(selector, context);
-    $.ajax = () => {};
-    $.templates = () => ({ render: () => {} });
-
+    $.ajax = () => { };
     const mockStore = configureMockStore([middleware(types, actions, $)]);
 
     it('createBookmark action should triggers bookmarkCreated and setReposAreSaved action', () => {
