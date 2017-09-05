9998d63dbfaf2649b1a9fe2ab9f4765fea0e0f61
Calvin Huang <calvin.peak@capslock.tw>
2017-09-04 20:01:27 -0700

    Wrap action as es6 module.

--- public/js/actions.js
+++ public/js/actions.js
@@ -1,88 +1,102 @@
-/**
- * Action types
- */
-const actionTypes = {
-  FETCH_PUBLIC_REPOS: 'FETCH_PUBLIC_REPOS',
-  RECEIVE_PUBLIC_REPOS: 'RECEIVE_PUBLIC_REPOS',
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+      (factory((global.actions = global.actions || {})));
+}(this, (function (exports) {
+  'use strict';
 
-  FETCH_REPOS_NEXT_PAGE: 'FETCH_REPOS_NEXT_PAGE',
+  /**
+   * Action types
+   */
+  const actionTypes = {
+    FETCH_PUBLIC_REPOS: 'FETCH_PUBLIC_REPOS',
+    RECEIVE_PUBLIC_REPOS: 'RECEIVE_PUBLIC_REPOS',
 
-  SET_REPOS_ARE_SAVED: 'SET_REPOS_ARE_SAVED',
+    FETCH_REPOS_NEXT_PAGE: 'FETCH_REPOS_NEXT_PAGE',
 
-  FETCH_BOOKMARKS: 'FETCH_BOOKMARKS',
-  RECEIVE_BOOKMARKS: 'RECEIVE_BOOKMARKS',
+    SET_REPOS_ARE_SAVED: 'SET_REPOS_ARE_SAVED',
 
-  TOGGLE_BOOKMARK: 'TOGGLE_BOOKMARK',
+    FETCH_BOOKMARKS: 'FETCH_BOOKMARKS',
+    RECEIVE_BOOKMARKS: 'RECEIVE_BOOKMARKS',
 
-  CREATE_BOOKMARK: 'CREATE_BOOKMARK',
-  BOOKMARK_CREATED: 'BOOKMARK_CREATED',
+    TOGGLE_BOOKMARK: 'TOGGLE_BOOKMARK',
 
-  DELETE_BOOKMARK: 'DELETE_BOOKMARK',
-  BOOKMARK_DELETED: 'BOOKMARK_DELETED',
+    CREATE_BOOKMARK: 'CREATE_BOOKMARK',
+    BOOKMARK_CREATED: 'BOOKMARK_CREATED',
 
-  SHOW_NOTIFICATION: 'SHOW_NOTIFICATION',
-}
+    DELETE_BOOKMARK: 'DELETE_BOOKMARK',
+    BOOKMARK_DELETED: 'BOOKMARK_DELETED',
 
-/**
- * Action creators
- */
-const actions = {
-  fetchPublicRepos: () => ({
-    type: actionTypes.FETCH_PUBLIC_REPOS,
-  }),
+    SHOW_NOTIFICATION: 'SHOW_NOTIFICATION',
+  }
 
-  receivePublicRepos: (repos) => ({
-    type: actionTypes.RECEIVE_PUBLIC_REPOS,
-    repos: repos,
-  }),
+  /**
+   * Action creators
+   */
+  const actions = {
+    fetchPublicRepos: () => ({
+      type: actionTypes.FETCH_PUBLIC_REPOS,
+    }),
 
-  fetchReposNextPage: () => ({
-    type: actionTypes.FETCH_REPOS_NEXT_PAGE,
-  }),
+    receivePublicRepos: (repos) => ({
+      type: actionTypes.RECEIVE_PUBLIC_REPOS,
+      repos: repos,
+    }),
 
-  setReposAreSaved: (bookmarkIds) => ({
-    type: actionTypes.SET_REPOS_ARE_SAVED,
-    bookmarkIds: bookmarkIds,
-  }),
+    fetchReposNextPage: () => ({
+      type: actionTypes.FETCH_REPOS_NEXT_PAGE,
+    }),
 
-  fetchBookmarks: () => ({
-    type: actionTypes.FETCH_BOOKMARKS,
-  }),
+    setReposAreSaved: (bookmarkIds) => ({
+      type: actionTypes.SET_REPOS_ARE_SAVED,
+      bookmarkIds: bookmarkIds,
+    }),
 
-  receiveBookmarks: (bookmarks) => ({
-    type: actionTypes.RECEIVE_BOOKMARKS,
-    bookmarks: bookmarks,
-  }),
+    fetchBookmarks: () => ({
+      type: actionTypes.FETCH_BOOKMARKS,
+    }),
 
-  toggleBookmark: (repoId) => ({
-    type: actionTypes.TOGGLE_BOOKMARK,
-    repoId: repoId,
-  }),
+    receiveBookmarks: (bookmarks) => ({
+      type: actionTypes.RECEIVE_BOOKMARKS,
+      bookmarks: bookmarks,
+    }),
 
-  createBookmark: (repoId, fullName) => ({
-    type: actionTypes.CREATE_BOOKMARK,
-    repoId: repoId,
-    fullName: fullName,
-  }),
+    toggleBookmark: (repoId) => ({
+      type: actionTypes.TOGGLE_BOOKMARK,
+      repoId: repoId,
+    }),
 
-  bookmarkCreated: (repoId, fullName) => ({
-    type: actionTypes.BOOKMARK_CREATED,
-    repoId: repoId,
-    fullName: fullName,
-  }),
+    createBookmark: (repoId, fullName) => ({
+      type: actionTypes.CREATE_BOOKMARK,
+      repoId: repoId,
+      fullName: fullName,
+    }),
 
-  deleteBookmark: (repoId) => ({
-    type: actionTypes.DELETE_BOOKMARK,
-    repoId: repoId,
-  }),
+    bookmarkCreated: (repoId, fullName) => ({
+      type: actionTypes.BOOKMARK_CREATED,
+      repoId: repoId,
+      fullName: fullName,
+    }),
 
-  bookmarkDeleted: (repoId) => ({
-    type: actionTypes.BOOKMARK_DELETED,
-    repoId: repoId,
-  }),
+    deleteBookmark: (repoId) => ({
+      type: actionTypes.DELETE_BOOKMARK,
+      repoId: repoId,
+    }),
 
-  showNotification: (message) => ({
-    type: actionTypes.SHOW_NOTIFICATION,
-    message: message,
-  }),
-}
+    bookmarkDeleted: (repoId) => ({
+      type: actionTypes.BOOKMARK_DELETED,
+      repoId: repoId,
+    }),
+
+    showNotification: (message) => ({
+      type: actionTypes.SHOW_NOTIFICATION,
+      message: message,
+    }),
+  }
+
+  exports.types = actionTypes;
+  exports.default = Object.assign(actions, { types: actionTypes });
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
