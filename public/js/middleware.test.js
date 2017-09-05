a520ca5289e95a44c4957f967df613ba14786576
Calvin Huang <calvin.peak@capslock.tw>
2017-09-04 21:50:55 -0700

    Add toggle bookmark test.

--- public/js/middleware.test.js
+++ public/js/middleware.test.js
@@ -54,5 +54,41 @@ describe('test middleware behavior', () => {
 
       expect(store.getActions()).toEqual(expectedActions);
     });
+
+    describe('toggleBookmark action will trigger bookmarkCreated or bookmarkDeleted action', () => {
+      it('when bookmark has been saved, trigger deleteBookmark relatived actions', () => {
+        const store = mockStore({
+          repos: { page: 1, data: [{ id: 1, full_name: 'foo/boo' }] },
+          bookmarks: [{ repo_id: 1, full_name: 'foo/boo' }],
+        });
+
+        const expectedActions = [
+          actions.toggleBookmark(1),
+          actions.deleteBookmark(1),
+          actions.bookmarkDeleted(1),
+          actions.setReposAreSaved([1]),
+        ];
+        store.dispatch(expectedActions[0]);
+
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+
+      it('when bookmark is not saved, trigger createBookmark relatived actions', () => {
+        const store = mockStore({
+          repos: { page: 1, data: [{ id: 1, full_name: 'foo/boo' }] },
+          bookmarks: [],
+        });
+
+        const expectedActions = [
+          actions.toggleBookmark(1),
+          actions.createBookmark(1, 'foo/boo'),
+          actions.bookmarkCreated(1, 'foo/boo'),
+          actions.setReposAreSaved([]),
+        ];
+        store.dispatch(expectedActions[0]);
+
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+    });
   });
 });
